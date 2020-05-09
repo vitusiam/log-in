6a1e45c86406997229395b4b393aaaf343111b80
vitusiam <62211136+vitusiam@users.noreply.github.com>
2020-05-10 00:30:28 +0800

    Add files via upload
    fixed logging process

--- learn.py
+++ learn.py
@@ -1,8 +1,34 @@
-test = open("user.txt", "r")
 
-print(test.readlines())
-for i in test.readlines():
-    print(test.readline())
+password = open("user.txt", "r").read().splitlines()
+dic = {}
+x = "123"
+y = "mars"
+for i, v in enumerate(password):
+    new = password[i].split(":")
+    dic[new[0]] = new[1]
+
+
+print(dic)
+if x in dic:
+    print(x)
+"""for i in dic:
+    if x == i:
+        if dic[x] == y:
+            print("right")
+        else:
+            print("wrong try again")
+            break"""
 
 
 
+"""this = open("user.txt", "r").read().splitlines()
+x = "vitus"
+y = "123"
+for i, v in enumerate(this):
+    new = this[i].split(":")
+    print(new)
+
+    if x == new[0] and y == new[1]:
+        print("logged in!")
+    else:
+        print("wrong")"""
--- login.py
+++ login.py
@@ -14,32 +14,47 @@ class User:
         check = input("password again: ")
         while password != check:
             print("Not the same, please check!")
-            check = input("password agian: ")
+            check = input("password again: ")
 
         user_file = open("user.txt", "a")
-        user_file.write(f"username: {username}, password: {password}\n\n")
+        user_file.write(f"{username}:{password}\n")
         user_file.close()
+        user_dic[username] = password
         print("You have created a account!")
 
     def del_account(self):
-        user_delete = input("Username:")
+        user_delete = input("Username: ")
 
     def log_in(self):
-        logfile = open("user.txt", "r").readlines()
-        while True:
+        logged_in = False
+        while not logged_in:
             username = input("Your username: ")
             if username == "quit":
-                return False
-            password = input("Your password: ")
-            if username in logfile and password in logfile:
-                print(f"Hi {username}, You have logged in!\n")
-                return False
-            elif username not in logfile or password not in logfile:
-                print("Wrong password or username!")
+                logged_in = True
+            elif username in user_dic:
+                right_password = False
+                while not right_password:
+                    password = input("Your password: ")
+                    if user_dic[username] == password:
+                        print(f"Hi {username}, you have logged in!\n")
+                        right_password = True
+                        logged_in = True
+                    elif password == "quit":
+                        right_password = True
+                    else:
+                        print("Wrong password!")
+            elif username not in user_dic:
+                print("Wrong username")
 
     def look_user_info(self):
         print("\n" + open("user.txt").read())
 
+
+user_dic = {}
+user_logfile = open("user.txt", "r").read().splitlines()
+for i, v in enumerate(user_logfile):
+    new = user_logfile[i].split(":")
+    user_dic[new[0]] = new[1]
 
 log = True
 while log:
